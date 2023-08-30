65ce169d7d98dafa331707ca5b4b182f3a3b8689
NegrobovKirill <negrobov.kir.1999@gmail.com>
2023-08-30 16:43:47 +0300

    First commit

---
+++ app/src/main/java/com/example/touch/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.touch
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.TypedValue
+import com.example.touch.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+
+
+        setContentView(bindingClass.root)
+
+
+        bindingClass.toucher.setOnClickListener {
+
+           generateRandomColor()
+
+        }
+    }
+
+    private fun generateRandomColor(){
+        val rnd = Random()
+        val colorBackGround = Color.argb(255, rnd.nextInt(256),rnd.nextInt(256),rnd.nextInt(256))
+        bindingClass.toucher.setBackgroundColor(colorBackGround)
+        val colorText = Color.argb(255, rnd.nextInt(256),rnd.nextInt(256),rnd.nextInt(256))
+        bindingClass.toucher.setTextColor(colorText)
+        val intRange:IntRange = 20..60
+        bindingClass.toucher.setTextSize(TypedValue.COMPLEX_UNIT_SP,intRange.random().toFloat())
+
+    }
+}
